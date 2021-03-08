9507a847980574eb1ca04afc33579faaedc5b085
Julian Maingot <julianmaingot@gmail.com>
2021-03-08 08:57:33 -0800

    working allocator!

--- src/main.zig
+++ src/main.zig
@@ -4,6 +4,34 @@ const c = @cImport({
     @cInclude("vulkan/vulkan.h");
 });
 
+// TODO hack to get around comptime alignment for now
+// https://github.com/ziglang/zig/issues/7172
+fn allocAligned(allocator: *std.mem.Allocator,
+                alignment: u29,
+                size: usize) std.mem.Allocator.Error![]align(1) u8 {
+    return switch (alignment) {
+        1 => alignedAllocator1.alloc(allocator, size),
+        2 => alignedAllocator2.alloc(allocator, size),
+        else => undefined,
+    };
+}
+
+const AlignedAllocator = struct {
+    alignment: u29,
+
+    const allocType = u8;
+    const exact = std.mem.Allocator.Exact.at_least;
+
+    fn alloc(comptime self: AlignedAllocator,
+             allocator: *std.mem.Allocator,
+             size: usize) std.mem.Allocator.Error![]align(1) u8 {
+        return allocator.allocAdvanced(allocType, self.alignment, size, exact);
+    }
+};
+
+const alignedAllocator1 = comptime AlignedAllocator { .alignment = 1 << 0, };
+const alignedAllocator2 = comptime AlignedAllocator { .alignment = 1 << 1, };
+
 // TODO actually use allocationScope
 fn vkAllocate(pUserData: ?*c_void,
               size: usize,
@@ -11,12 +39,7 @@ fn vkAllocate(pUserData: ?*c_void,
               allocationScope: c.VkSystemAllocationScope) callconv(.C) ?*c_void {
     if (pUserData) |justUserData| {
         var allocator = @ptrCast(*std.mem.Allocator, @alignCast(8, justUserData));
-        const truncatedAlignment = @truncate(u29, alignment);
-        // TODO hack to get around comptime alignment for now
-        // https://github.com/ziglang/zig/issues/7172
-        const actualAlignment:u29 = 8;
-        std.debug.assert(truncatedAlignment <= 8);
-        if (allocator.allocAdvanced(u8, actualAlignment, size, std.mem.Allocator.Exact.at_least)) |res| {
+        if (allocAligned(allocator, @truncate(u29, alignment), size)) |res| {
             return @as(*c_void, &res[0]);
         } else |err| {
             std.log.err("alloc error {}", .{err});
