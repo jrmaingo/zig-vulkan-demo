b6db5f571e19f7031d664120a390ad5e0b66f06c
Julian Maingot <julianmaingot@gmail.com>
2021-03-08 22:55:55 -0800

    Formatting

--- src/main.zig
+++ src/main.zig
@@ -6,9 +6,7 @@ const c = @cImport({
 
 // TODO hack to get around comptime alignment for now
 // https://github.com/ziglang/zig/issues/7172
-fn allocAligned(allocator: *std.mem.Allocator,
-                alignment: u29,
-                size: usize) std.mem.Allocator.Error![]align(1) u8 {
+fn allocAligned(allocator: *std.mem.Allocator, alignment: u29, size: usize) std.mem.Allocator.Error![]align(1) u8 {
     // TODO go up to 29
     return switch (alignment) {
         1 << 0 => alignedAllocators[0].alloc(allocator, size),
@@ -30,28 +28,23 @@ const AlignedAllocator = struct {
     const exact = std.mem.Allocator.Exact.at_least;
 
     // implictly cast to alignment of 1 since we use one fn signature for all alloc calls
-    fn alloc(comptime self: AlignedAllocator,
-             allocator: *std.mem.Allocator,
-             size: usize) std.mem.Allocator.Error![]align(1) u8 {
+    fn alloc(comptime self: AlignedAllocator, allocator: *std.mem.Allocator, size: usize) std.mem.Allocator.Error![]align(1) u8 {
         return allocator.allocAdvanced(allocType, self.alignment, size, exact);
     }
 };
 
 // this generates structs for all possible alignment values
 const alignedAllocators = init: {
-    const count = 29;   // since alignment is u29
+    const count = 29; // since alignment is u29
     var values: [count]AlignedAllocator = undefined;
     for (values) |*value, i| {
-        value.* = AlignedAllocator { .alignment = 1 << i };
+        value.* = AlignedAllocator{ .alignment = 1 << i };
     }
     break :init values;
 };
 
 // TODO actually use allocationScope
-fn vkAllocate(pUserData: ?*c_void,
-              size: usize,
-              alignment: usize,
-              allocationScope: c.VkSystemAllocationScope) callconv(.C) ?*c_void {
+fn vkAllocate(pUserData: ?*c_void, size: usize, alignment: usize, allocationScope: c.VkSystemAllocationScope) callconv(.C) ?*c_void {
     if (pUserData) |justUserData| {
         var allocator = @ptrCast(*std.mem.Allocator, @alignCast(8, justUserData));
         if (allocAligned(allocator, @truncate(u29, alignment), size)) |res| {
@@ -72,7 +65,7 @@ pub fn main() anyerror!void {
     var allocator = &arena.allocator;
 
     // TODO rest of allocator
-    const vkAllocationCallbacks = c.VkAllocationCallbacks {
+    const vkAllocationCallbacks = c.VkAllocationCallbacks{
         .pUserData = allocator,
         .pfnAllocation = vkAllocate,
         .pfnReallocation = undefined,
@@ -81,12 +74,12 @@ pub fn main() anyerror!void {
         .pfnInternalFree = undefined,
     };
 
-//    var vkInstance = c.VkInstance {};
-//    const res = c.vkCreateInstance(createInfo, vkAllocator, vkInstance);
-//    defer c.vkDestroyInstance(vkInstance, vkAllocator);
-//    if (res != 0) {
-//        std.log.error("init failed!", .{});
-//    }
+    //    var vkInstance = c.VkInstance {};
+    //    const res = c.vkCreateInstance(createInfo, vkAllocator, vkInstance);
+    //    defer c.vkDestroyInstance(vkInstance, vkAllocator);
+    //    if (res != 0) {
+    //        std.log.error("init failed!", .{});
+    //    }
 
     std.log.info("done", .{});
 }
